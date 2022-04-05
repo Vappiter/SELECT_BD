08fce72fb282c298b8099c60ee3fd0c30ac49408
Andrey Vladimirov <vappiter@yandex.ru>
2022-04-05 22:19:47 +0300

    WORK day 2

--- select_BD.py
+++ select_BD.py
@@ -5,6 +5,15 @@ import os
 
 data_bd = []
 
+
+def insert_table(colums,len_list):
+  inrt_data_bd = []
+  for i1 in range (1, len_list):
+    inrt_data_bd.append(data_bd[i1][colums])
+  var_test1 = set (inrt_data_bd)
+  pprint(var_test1)
+  
+
 file_path = (os.path.split(__file__)) # Считывает текущую директорию скрипта, тут же должен храниться файл CSV
 os.chdir(file_path [0]) # Устанавливает директорию
 with open("file_BD.csv") as f:
@@ -18,17 +27,30 @@ connection = engine.connect()
 # print (engine)
 # connection.execute('''INSERT INTO album(album_id, album_name, album_year) \
 #                    VALUES (4, 'ALBUM 4', 1980), (5, 'ALBUM 5', 1990), (6, 'ALBUM 6', 1990)''')
+len_list = len (data_bd)
+insert_table(0, len_list)
+insert_table(1, len_list)
+insert_table(3, len_list)
 
-for data_bd_2 in data_bd:
-  musician_test = connection.execute('''SELECT musician_name FROM musician;''')
-  if musician_test.fetchone() == None:
-      sql_text = '''INSERT INTO musician (musician_name, musician_nickname) VALUES (%s, %s)'''
-      data_insert = data_bd_2[0], data_bd_2[1]
-      connection.execute(sql_text, data_insert)
-  else:
-      pprint ('УРА!!! Работает')  
-  pprint (musician_test)
+# for data_bd_2 in data_bd:
+#     if data_bd_2[0] == 'musician_name':
+#      continue   
+#     musician_test = connection.execute('''SELECT musician_name FROM musician;''')
+#     var1 = musician_test.fetchone()
+#     if var1 == None:
+#       sql_text = '''INSERT INTO musician (musician_name, musician_nickname) VALUES (%s, %s)'''
+#       data_insert = data_bd_2[0], data_bd_2[1]
+#       connection.execute(sql_text, data_insert)
+#     elif var1 == data_bd_2[0]:
+#         continue
+#     else:
+#       sql_text = '''INSERT INTO musician (musician_name, musician_nickname) VALUES (%s, %s)'''
+#       data_insert = data_bd_2[0], data_bd_2[1]
+#       connection.execute(sql_text, data_insert)   
+              
+            
+# pprint (musician_test)
   
     
-sel = connection.execute('''SELECT * FROM album;''').fetchmany(6)
-print (sel)
+# sel = connection.execute('''SELECT * FROM album;''').fetchmany(6)
+# print (sel)
